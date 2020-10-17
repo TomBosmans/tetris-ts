ef72362c7c65baa41a5e8ee03aea05c42d873cf7
Tom Bosmans <tom.94.bosmans@gmail.com>
2020-10-17 17:51:17 +0200

    add useInterval hook

---
+++ src/hooks/useInterval.ts
@@ -0,0 +1,21 @@
+import { useEffect, useRef } from "react"
+
+type CallbackFunction = () => void
+
+export default function useInterval(callback: CallbackFunction, delay: number) {
+  const savedCallback = useRef<CallbackFunction>()
+
+  // Remember the latest callback.
+  useEffect(() => {
+    savedCallback.current = callback
+  }, [callback])
+
+  // Set up the interval.
+  useEffect(() => {
+    if (delay === null) return
+
+    const tick = () => savedCallback.current && savedCallback.current()
+    const id = setInterval(tick, delay)
+    return () => clearInterval(id)
+  }, [delay])
+}
