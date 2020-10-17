8677df944a25eef413fde7a98725ef78dc91545c
Tom Bosmans <tom.94.bosmans@gmail.com>
2020-10-17 17:47:52 +0200

    add types

---
+++ src/types/index.ts
@@ -0,0 +1,27 @@
+export type Cell = " " | "o" | "s" | "z" | "i" | "j" | "t" | "l"
+export type Row = Cell[]
+export type Grid = Row[]
+export type Block = {
+  shape: string
+  offsetX: number
+  offsetY: number
+  rotation: number
+}
+
+export type Action = {
+  type: ActionType
+  payload?: Record<any, any>
+}
+
+export type ActionType = "MOVE_BLOCK_DOWN" | "MOVE_BLOCK_LEFT" | "MOVE_BLOCK_RIGHT" | "ROTATE_BLOCK_LEFT" | "ROTATE_BLOCK_RIGHT"
+
+export type Keybindings = Record<number, ActionType>
+
+export type PlayfieldState = {
+  level: number
+  score: number
+  grid: Grid
+  currentBlock: Block
+  nextBlock: Block
+  keybindings: Keybindings
+}
